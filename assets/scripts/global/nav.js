7ef8952b7a6a77391a3a59b05c7753628e9417b5
summertides <fdfgehge@gmail.com>
2023-08-10 22:51:19 -0400

    finished nav menu ish

--- assets/scripts/global/nav.js
+++ assets/scripts/global/nav.js
@@ -1,3 +1,9 @@
 const toggleNav = () => {
     document.body.dataset.nav = document.body.dataset.nav === "true" ? "false" : "true";
 }
+
+const borgerToggle = () => {
+    var ham = document.getElementById("hamex");
+
+    ham.classList.toggle('active');
+}
