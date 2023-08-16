a6a3d46fd08c6c79757137a3104c0a2a3de191e6
summertides <fdfgehge@gmail.com>
2023-08-16 09:56:07 -0400

    design changes 3
    changes to some design, very obvious. +footer

--- assets/scripts/index/index.js
+++ assets/scripts/index/index.js
@@ -1,3 +1,11 @@
 const logo = document.getElementById("logo");
 
-var y = window.scrollY;
+window.addEventListener("scroll", function() {
+    if (this.window.scrollY > 30) {
+        logo.classList.add("logo")
+        logo.classList.remove("menulogo")
+    } else {
+        logo.classList.add("menulogo")
+        logo.classList.remove("logo")
+    }
+})
